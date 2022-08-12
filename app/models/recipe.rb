55983429df2ab57376a53274fe8e91ea838b7f8b
Brian Robertson <brianrobertso42@gmail.com>
2022-08-11 21:04:48 -0400

    initial commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+	belongs_to :user
+
+	has_many :ingredients, dependent: :destroy
+	accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+
+	has_many :steps, dependent: :destroy
+	accepts_nested_attributes_for :steps, reject_if: :all_blank, allow_destroy: true
+end
